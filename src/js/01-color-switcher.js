01c8d84cf21e0778f38a1f383e97ae8f55ca5bf6
shkurkoka <shkurkoka@gmail.com>
2023-06-02 18:15:36 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,23 +1,27 @@
 const body = document.querySelector('body');
-const start = document.querySelector('[data-start]');
-const stop = document.querySelector('[data-stop]');
+const startRand = document.querySelector('[data-start]');
+const stopRand = document.querySelector('[data-stop]');
+stopRand.setAttribute("disabled", true);
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
 }
  
-start.addEventListener("click", () => {
+let timerId;
+
+
+startRand.addEventListener("click", () => {
     timerId = setInterval(() => {
         body.style.backgroundColor = `${getRandomHexColor()}`;
     }, 1000);
-    start.setAttribute("disabled", true);
-    if (stop.hasAttribute("disabled")) {
-        stop.removeAttribute("disabled");
+    startRand.setAttribute("disabled", true);
+    if (stopRand.hasAttribute("disabled")) {
+        stopRand.removeAttribute("disabled");
     }
 });
 
-stop.addEventListener("click", () => {
+stopRand.addEventListener("click", () => {
     clearInterval(timerId);
-    start.removeAttribute("disabled");
-    stop.setAttribute("disabled", true);
+    startRand.removeAttribute("disabled");
+    stopRand.setAttribute("disabled", true);
 }); 
